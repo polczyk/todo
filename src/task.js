4e9ea953aa636c12101ef78375807058a9d542cf
radek-desktop <dolorismachina@gmail.com>
2018-09-29 13:46:52 +0100

    Task flex-box patch

--- src/task.js
+++ src/task.js
@@ -18,17 +18,25 @@ class Task {
 
     createElement() {
         const element = document.createElement('li');
+        element.classList.add('task');
 
+        const div1 = document.createElement('div');
+        div1.classList.add('flex-left');
         const span = document.createElement('span');
         span.innerText = this.name;
         span.addEventListener('click', e => this.toggle());
+        div1.appendChild(span);
+        element.appendChild(div1);
 
+        const div2 = document.createElement('div');
+        div2.classList.add('flex-right');
         const button = document.createElement('button');
-        button.innerText = 'Remove';
         button.addEventListener('click', e => this.remove(this));
         const icon = document.createElement('i');
         icon.classList.add('fas', 'fa-times');
         button.appendChild(icon);
+        div2.appendChild(button);
+        element.appendChild(div2);
 
         element.dataset.id = this.id;
         element.appendChild(span);
